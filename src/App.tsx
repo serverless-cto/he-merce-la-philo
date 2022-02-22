e599ab5368afe8e9330da9c796f0d65378ba1c42
Marc-Antoine Lebourgeois <marc.antoine.lebourgeois@gmail.com>
2022-02-22 09:37:30 +0100

    style: apply eslint on template components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const test = "test";
   return (
     <div className="App">
       <header className="App-header">
